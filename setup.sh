764938201b3384e6266883a5445f612937eaff5c
aeea-0605 <tmdwnrj96@gmail.com>
2021-08-12 20:19:28 +0900

    modify setup file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+# Scrapy project 폴더 생성
 rm -rf naver_movie
 scrapy startproject naver_movie
 
-cd naver_movie/naver_movie/spiders
+# spider.py 생성
+cd naver_movie
 scrapy genspider NaverMovie movie.naver.com/movie/running/current.naver
 cd ../../../
