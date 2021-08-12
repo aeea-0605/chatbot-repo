07db64fc9d179071b5d6e132bcdebcf8b21fa86c
aeea-0605 <tmdwnrj96@gmail.com>
2021-08-12 20:20:31 +0900

    add filter options

--- naver_movie/naver_movie/pipelines.py
+++ naver_movie/naver_movie/pipelines.py
@@ -1,6 +1,28 @@
+import datetime
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class NaverMoviePipeline:
+
+    def __init__(self):
+        pass
+
+    def open_spider(self, spider):
+        spider.logger.info("Pipeline Started.")
+
+
     def process_item(self, item, spider):
-        return item
+
+        if item.get("rate") != "0":
+            item["is_pass"] = True
+            item["crawled_time"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            return item
+        else:
+            raise DropItem(f'Dropped Item. This Rate is {item.get("rate")}')
+
+
+
+    def close_spider(self, spider):
+        spider.logger.info("Pipeline Closed.")
