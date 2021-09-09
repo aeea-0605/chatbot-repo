ecd8b9b7afe3d7e4b78b618fea7a9967c2b9b737
aeea-0605 <tmdwnrj96@gmail.com>
2021-09-09 15:29:13 +0900

    scrapy pipeline code

--- naver_movie/naver_movie/pipelines.py
+++ naver_movie/naver_movie/pipelines.py
@@ -1,16 +1,32 @@
 import datetime
-import MySQL
+import MySQLdb
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
+from naver_movie.config import db_info
 
 
 class NaverMoviePipeline:
 
     def __init__(self):
-        pass
+        self.conn = MySQLdb.connect(**db_info)
+        self.curs = self.conn.cursor()
 
     def open_spider(self, spider):
         spider.logger.info("Pipeline Started.")
+        QUERY = """
+CREATE TABLE IF NOT EXISTS naver_movie (
+id INT PRIMARY KEY AUTO_INCREMENT, 
+title TEXT, 
+link TEXT,
+rate FLOAT,
+genre TEXT, 
+score FLOAT,
+view FLOAT,
+director TEXT,
+actor TEXT,
+crawled_time TEXT);
+"""
+        self.curs.execute(QUERY.replace("\n", ""))
 
 
     def process_item(self, item, spider):
@@ -19,6 +35,9 @@ class NaverMoviePipeline:
             # item["is_pass"] = True
             item["crawled_time"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+            self.curs.execute(f'INSERT INTO naver_movie (title, link, rate, genre, score, view, director, actor, crawled_time) VALUES ("{item.get("title")}", "{item.get("link")}", {item.get("rate")}, "{item.get("genre")}", {item.get("score")}, {item.get("view")}, "{item.get("director")}", "{item.get("actor")}", "{item.get("crawled_time")}");')
+
+            spider.logger.info("Item to DB inserted.")
             return item
         else:
             raise DropItem(f'Dropped Item. This Rate is {item.get("rate")}')
@@ -27,3 +46,6 @@ class NaverMoviePipeline:
 
     def close_spider(self, spider):
         spider.logger.info("Pipeline Closed.")
+
+        self.conn.commit()
+        self.conn.close()
