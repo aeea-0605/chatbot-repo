321fed3d8645714b1e34cd4bc6ee14f3074e57bb
aeea-0605 <tmdwnrj96@gmail.com>
2021-09-09 15:42:13 +0900

    setting scrapy project

---
+++ setup_scrapy.sh
@@ -0,0 +1,8 @@
+# Scrapy project 폴더 생성
+rm -rf naver_movie
+scrapy startproject naver_movie
+
+# spider.py 생성
+cd naver_movie
+scrapy genspider NaverMovie movie.naver.com/movie/running/current.naver
+cd ../../../
