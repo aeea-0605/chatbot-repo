6388ae302c425c42d0606aae3e2965d96d3e3e5c
aeea-0605 <tmdwnrj96@gmail.com>
2021-09-10 17:53:41 +0900

    chatbot of app running code

---
+++ chatbot/app/routes/bot.py
@@ -0,0 +1,54 @@
+from flask import request, Response
+from app import app
+from app.libs import naver
+from app.libs import forecast
+from app.libs import slack
+from app.config import ApiKey
+from app.items.mysql_movie import NaverMovie
+
+
+@app.route("/")
+def index():
+    return "server is running!"
+
+
+@app.route("/bot", methods=["POST"])
+def bot():
+    trigger_word = request.form.get('trigger_word')
+    text = request.form.get('text')
+    text = text.replace(trigger_word, "").strip()
+
+    if text.find(":") <= 0:
+        msg = '[command]:[data] 포맷으로 입력해주세요. \n챗봇의 기능을 알고싶다면 "$bot 도움:" 을 입력해주세요.'
+        slack.send_msg(ApiKey.WEBHOOK_URL, msg)
+        return Response(), 200
+
+    command, data = text.split(":")[0], text.split(":")[1]
+    if command == "날씨":
+        params = ApiKey.get_json(["naver", "kakao", "weather"])
+        params.update({"addr": data})
+        msg = forecast.run(**params)
+    elif command == "번역":
+        params = ApiKey.get_json(["naver"])
+        params.update({"msg": data})
+        msg = naver.translate(**params)
+    elif command == "영화":
+        datas = NaverMovie.query.order_by(NaverMovie.rate.desc()).limit(int(data))
+        msg = f'<수집시점 : {datas[0].crawled_time.split(" ")[0]}>\n'
+        for i in range(int(data)):
+            msg += f'{i+1}. {datas[i].title} (예매율: {datas[i].rate}%, 평점: {datas[i].score}) \n'
+            msg += f'\t {datas[i].link}\n'
+            if i != (int(data) - 1):
+                msg += "-------------------\n"
+    elif command == "도움":
+        msg = """
+챗봇 실행 명령어 >> $bot [command]:[data]
+command : 날씨, data : 날씨를 알고 싶은 지역명
+command : 번역, data : 번역하고 싶은 영문(english > korean)
+command : 영화, data : 현재상영작 중 보고 싶은 마지막 순위 입력
+        """
+    else:
+        msg = f'{command}는 없는 명령입니다. 현재 가능한 명령어는 날씨, 번역, 영화, 도움 입니다.'
+
+    slack.send_msg(ApiKey.WEBHOOK_URL, msg)
+    return Response(), 200
