51cdd378e49a64ed626678e200ec87773d6e5495
aeea-0605 <tmdwnrj96@gmail.com>
2021-09-10 17:08:25 +0900

    move path

--- chatbot/libs/slack.py
+++
@@ -1,7 +0,0 @@
-import requests, json
-
-
-def send_msg(WEBHOOK_URL, msg, channel="#dss", username="bot"):
-    payload = {"channel": channel, "username": username, "text": msg}
-    
-    return requests.post(WEBHOOK_URL, json.dumps(payload))
