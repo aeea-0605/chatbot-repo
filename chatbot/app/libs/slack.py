3c39f6d4a799611692b9f73c889bf4d8fcc0c068
aeea-0605 <tmdwnrj96@gmail.com>
2021-09-23 20:42:08 +0900

    send message to slack, using webhook_url

--- chatbot/app/libs/slack.py
+++ chatbot/app/libs/slack.py
@@ -2,6 +2,12 @@ import requests, json
 
 
 def send_msg(WEBHOOK_URL, msg, channel="#dss", username="bot"):
+    """
+    send_msg function : 메세지를 slack work space에 전송해주는 함수
+    input arguments : slack_webhook_url, message
+                     , channel(default : #dss), username(default : bot)
+    return <Response [200]>, if send_msg succeed.
+    """
     payload = {"channel": channel, "username": username, "text": msg}
     
     return requests.post(WEBHOOK_URL, json.dumps(payload))
