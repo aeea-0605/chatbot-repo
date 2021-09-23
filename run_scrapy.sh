e8419d7f05de4722fdc3f5b8c9fb6330fe2d926e
aeea-0605 <tmdwnrj96@gmail.com>
2021-09-23 20:31:22 +0900

    run naver_movie scrapying code

--- run_scrapy.sh
+++ run_scrapy.sh
@@ -1,3 +1,6 @@
-rm naver_movie/result.csv
-cd naver_movie
+# scrapy.cfg 가 있는 디렉토리로 이동
+cd /home/ubuntu/notebooks/chatbot-repo/naver_movie
+
+# result.csv가 있으면 제거하고 scrapying 시작
+rm result.csv
 scrapy crawl NaverMovie -o result.csv
