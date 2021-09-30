df56056ed75203c01d52548d8e4bb1efe26b8ef4
aeea-0605 <tmdwnrj96@gmail.com>
2021-09-30 14:22:50 +0900

    scrapy pipeline (add log msg)

--- naver_movie/naver_movie/pipelines.py
+++ naver_movie/naver_movie/pipelines.py
@@ -13,6 +13,7 @@ class NaverMoviePipeline:
 
     def open_spider(self, spider):
         spider.logger.info("Pipeline Started.")
+        spider.logger.info("Scrapying Start")
         QUERY = """
 DROP TABLE naver_movie;
 
@@ -38,15 +39,16 @@ crawled_time TEXT);
 
             self.curs.execute(f'INSERT INTO naver_movie (title, link, rate, genre, score, view, director, actor, crawled_time) VALUES ("{item.get("title")}", "{item.get("link")}", {item.get("rate")}, "{item.get("genre")}", {item.get("score")}, {item.get("view")}, "{item.get("director")}", "{item.get("actor")}", "{item.get("crawled_time")}");')
 
-            spider.logger.info("Item to DB inserted.")
+            spider.logger.info(f'Item to DB inserted. title : {item.get("title")}')
             return item
         else:
-            raise DropItem(f'Dropped Item. This Rate is {item.get("rate")}')
+            raise DropItem(f'Dropped Item. title : {item.get("title")}')
 
 
 
     def close_spider(self, spider):
         spider.logger.info("Pipeline Closed.")
+        spider.logger.info("Scrapying Done")
 
         self.conn.commit()
         self.conn.close()
